6248082471f0fa33426ea8c6b796deb548f5f19d
Fatlum <fatlum@itsfatlum.de>
2026-02-03 09:24:58 +0100

    Fixex a bug where the device name would be included in the account id sensor.

--- custom_components/nintendo_switch_presence/sensor.py
+++ custom_components/nintendo_switch_presence/sensor.py
@@ -1,34 +1,11 @@
 from typing import Any, Optional
-import logging
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers import entity_registry as er
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    # Migrate entity registry unique_ids from "nintendo_switch_status_*" to
-    # "nintendo_switch_presence_*" so existing entities keep their settings.
-    ent_reg = er.async_get(hass)
-    migration_map = {
-        "nintendo_switch_status_status": "nintendo_switch_presence_status",
-        "nintendo_switch_status_game": "nintendo_switch_presence_game",
-        "nintendo_switch_status_profile": "nintendo_switch_presence_profile",
-        "nintendo_switch_status_account_id": "nintendo_switch_presence_account_id",
-        "nintendo_switch_status_total_playtime": "nintendo_switch_presence_total_playtime",
-        "nintendo_switch_status_platform": "nintendo_switch_presence_platform",
-    }
-    for old_unique, new_unique in migration_map.items():
-        entity_id = ent_reg.async_get_entity_id("sensor", DOMAIN, old_unique)
-        if entity_id:
-            ent_reg.async_update_entity(entity_id, new_unique_id=new_unique)
-            _LOGGER.debug(
-                "Migrated entity %s unique_id %s -> %s", entity_id, old_unique, new_unique
-            )
-
     coordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities(
         [
@@ -141,6 +118,7 @@ class NintendoSwitchProfileSensor(BaseNintendoSwitchSensor):
 
 
 class NintendoSwitchAccountIdSensor(BaseNintendoSwitchSensor):
+    has_entity_name = False
     _attr_unique_id = "nintendo_switch_presence_account_id"
     _attr_name = "Nintendo Switch Account ID"
     _attr_icon = "mdi:identifier"
